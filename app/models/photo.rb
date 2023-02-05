02900965251bc669ffa06984a6b9fa14fe47ffab
mib94 <mibnobachir@chicagobooth.edu>
2023-02-05 01:20:17 +0000

    Sample data created

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -22,13 +22,20 @@
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
-  has_many :fans, through: :likes
+  has_many :fans, through: :likes, dependent: :destroy
 
   validates :caption, presence: true
 
   validates :image, presence: true
+
+  scope :by_likes, -> { order(likes_count: :desc) }
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+
+  def fan_list
+    @_fan_list ||= fans.pluck(:username).to_sentence
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,21 +28,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id", dependent: :destroy
 
-  has_many :comments, foreign_key: "author_id"
+  has_many :comments, foreign_key: "author_id", dependent: :destroy
 
-  has_many :sent_follow_requests, foreign_key: "sender_id", class_name: "FollowRequest"
+  has_many :sent_follow_requests, foreign_key: "sender_id", class_name: "FollowRequest", dependent: :destroy
 
-  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: "sender_id", class_name: "FollowRequest"
+  has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: "sender_id", class_name: "FollowRequest", dependent: :destroy
 
-  has_many :received_follow_requests, foreign_key: "recipient_id", class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: "recipient_id", class_name: "FollowRequest", dependent: :destroy
 
-  has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: "recipient_id", class_name: "FollowRequest"
+  has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: "recipient_id", class_name: "FollowRequest", dependent: :destroy
 
-  has_many :likes, foreign_key: "fan_id"
+  has_many :likes, foreign_key: "fan_id", dependent: :destroy
 
-  has_many :liked_photos, through: :likes, source: :photo
+  has_many :liked_photos, through: :likes, source: :photo, dependent: :destroy
 
   has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
 
@@ -52,7 +52,7 @@ class User < ApplicationRecord
 
   has_many :discover, through: :leaders, source: :liked_photos
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 
   scope :past_week, -> { where(created_at: 1.week.ago...) }
 
