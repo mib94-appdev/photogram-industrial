8bf2b631be8bcf45ead9c8b9eb6a137c5e9e6fc9
mib94 <mibnobachir@chicagobooth.edu>
2023-02-05 18:43:40 +0000

    Added photo partial

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
 
-  get "/:username" => "users#show"
+  get ":username/liked" => "photos#liked", as: :liked_photos
+
+  get ":username" => "users#show", as: :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
